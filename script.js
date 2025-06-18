a5f1a7f31452ab30526d6159808c6076e059e37a
Edis <rahic.edis@gmail.com>
2025-06-18 19:41:50 -0400

    For loop now inside of a function that will take defaultGrid as parameter. Changing grid now works by inputting numeric value by user via a prompt.

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ console.log(gridContainer)
 
 // Button to change the size of the grid (only limited up to 100)
 const gridButton = document.getElementById("rightKnob")
-gridButton.click()
 console.log(gridButton)
 
 //Reset button
@@ -17,22 +16,40 @@ console.log(resetButton)
 // Set the default grid to 16 x 16 when app is rendered
 let defaultGrid = 16
 
+// Change grid button will prompt the user 
+// to enter a number (not exceeding 100)
+// and have the current div be removed
+// in exchange for the new grid number
+// the user entered
+gridButton.addEventListener("click", function() {
+    const gridChange = prompt("how would you like your grid to look like?")
+    defaultGrid = Number(gridChange)
+    console.log("Grid Change",gridChange)
+    console.log("Default Grid", defaultGrid)
+    generateGrid(defaultGrid);
+})
 
 // Use a for loop to generate new divs for grid-container
-for (i = 0; i < defaultGrid * defaultGrid; i++) {
+//Check if user entered their own grid number through prompt
 
-    // Create a variable that will generate a div
-    // These div's will generate until the for loop meets its condition
-    const gridCell = document.createElement(`div`);
-    gridCell.classList.add("grid-cell");
+function generateGrid(defaultGrid) {
+    console.log("before wipe", gridContainer.children.length)
+    gridContainer.innerHTML=""
+    console.log("After wipe", gridContainer.children.length)
+    for (i = 0; i < defaultGrid * defaultGrid; i++) {
 
-    // The grid-cell class will be appended as a child to the 
-    // containerGrid in order for the cells to be displayed
-    gridContainer.appendChild(gridCell);
+        // Create a variable that will generate a div
+        // These div's will generate until the for loop meets its condition
+        const gridCell = document.createElement(`div`);
+        gridCell.classList.add("grid-cell");
 
-    // whenever the user clicks on a grid-cell, the background colour 
-    // should be turned to black
-    gridCell.addEventListener("mousemove", function() {
+        // The grid-cell class will be appended as a child to the 
+        // containerGrid in order for the cells to be displayed
+        gridContainer.appendChild(gridCell);
+
+        // whenever the user clicks on a grid-cell, the background colour 
+        // should be turned to black
+        gridCell.addEventListener("mousemove", function() {
         gridCell.classList.add("hover");
     })
 
@@ -40,4 +57,18 @@ for (i = 0; i < defaultGrid * defaultGrid; i++) {
     resetButton.addEventListener("click", function(){
         gridCell.classList.remove("hover")
     })
-}
+
+    // If change grid button is clicked and user enters a prompt
+    // remove the current divs in exchange for the new grid number
+}}
+
+generateGrid(defaultGrid);
+
+//Add a button on the top of the screen 
+// that will send the user a popup asking 
+// for the number of squares per side for 
+// the new grid. Once entered, the existing 
+// grid should be removed, and a new grid 
+// should be generated in the same total 
+// space as before (e.g., 960px wide) so 
+// that you’ve got a new sketch pad.
