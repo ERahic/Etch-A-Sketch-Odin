12be959719e9b1377a5664890c625e9abf7912f2
Edis <rahic.edis@gmail.com>
2025-06-19 11:54:52 -0400

    Removed hardcoded width for .grid-cell. Added dynamic width and height size control for grid-cells inside of generateGrid function. Added a check if the user is choosing a number between 2 and 100, the user will receive an alert if they go beyond those boundries.

--- script.js
+++ script.js
@@ -1,74 +1,76 @@
 // Create a webpage that allows the user to draw on the grid using their mouse.
 
-
-// Set the default grid to 16x16 when the webpage is first rendered
-const gridContainer = document.getElementById("grid-container")
-console.log(gridContainer)
+// Capture grid container element
+const gridContainer = document.getElementById("grid-container");
+console.log(gridContainer);
 
 // Button to change the size of the grid (only limited up to 100)
-const gridButton = document.getElementById("rightKnob")
-console.log(gridButton)
+const gridButton = document.getElementById("rightKnob");
+console.log(gridButton);
 
 //Reset button
-resetButton = document.getElementById("leftKnob")
-console.log(resetButton)
+resetButton = document.getElementById("leftKnob");
+console.log(resetButton);
 
 // Set the default grid to 16 x 16 when app is rendered
-let defaultGrid = 16
+let defaultGrid = 16;
 
-// Change grid button will prompt the user 
-// to enter a number (not exceeding 100)
+// Change grid button will prompt the user
+// to enter a number (not exceeding 100 & not below 3)
 // and have the current div be removed
 // in exchange for the new grid number
 // the user entered
-gridButton.addEventListener("click", function() {
-    const gridChange = prompt("how would you like your grid to look like?")
-    defaultGrid = Number(gridChange)
-    console.log("Grid Change",gridChange)
-    console.log("Default Grid", defaultGrid)
+gridButton.addEventListener("click", function () {
+  const gridChange = prompt("how would you like your grid to look like?");
+  if (gridChange < 2 || gridChange > 100) {
+    alert("Error! Please choose between 2 and 100");
+  } else {
+    defaultGrid = Number(gridChange);
+    console.log("Grid Change", gridChange);
+    console.log("Default Grid", defaultGrid);
     generateGrid(defaultGrid);
-})
+  }
+});
 
 // Use a for loop to generate new divs for grid-container
 //Check if user entered their own grid number through prompt
 
 function generateGrid(defaultGrid) {
-    console.log("before wipe", gridContainer.children.length)
-    gridContainer.innerHTML=""
-    console.log("After wipe", gridContainer.children.length)
-    for (i = 0; i < defaultGrid * defaultGrid; i++) {
+  console.log("before wipe", gridContainer.children.length);
+  gridContainer.innerHTML = "";
+  console.log("After wipe", gridContainer.children.length);
 
-        // Create a variable that will generate a div
-        // These div's will generate until the for loop meets its condition
-        const gridCell = document.createElement(`div`);
-        gridCell.classList.add("grid-cell");
+  // Cell grid size to make sure that width of grid container
+  // will be divided by the defualtGrid variable,
+  // even when it changes by the user
+  const gridCellSize = 342 / defaultGrid;
 
-        // The grid-cell class will be appended as a child to the 
-        // containerGrid in order for the cells to be displayed
-        gridContainer.appendChild(gridCell);
+  for (i = 0; i < defaultGrid * defaultGrid; i++) {
+    // Create a variable that will generate a div
+    // These div's will generate until the for loop meets its condition
+    const gridCell = document.createElement(`div`);
+    gridCell.classList.add("grid-cell");
 
-        // whenever the user clicks on a grid-cell, the background colour 
-        // should be turned to black
-        gridCell.addEventListener("mousemove", function() {
-        gridCell.classList.add("hover");
-    })
+    // Set the width and height of grid cells by
+    // matching it with the cellSize (342 / defaultGrid)
+    gridCell.style.width = `${gridCellSize}px`;
+    gridCell.style.height = `${gridCellSize}px`;
+
+    // The grid-cell class will be appended as a child to the
+    // containerGrid in order for the cells to be displayed
+    gridContainer.appendChild(gridCell);
+
+    // whenever the user hovers over a grid-cell, the background colour
+    // should be turned to black
+    gridCell.addEventListener("mousemove", function () {
+      gridCell.classList.add("hover");
+    });
 
     // Reset the cell's back to white when clicking on reset button
-    resetButton.addEventListener("click", function(){
-        gridCell.classList.remove("hover")
-    })
-
-    // If change grid button is clicked and user enters a prompt
-    // remove the current divs in exchange for the new grid number
-}}
+    resetButton.addEventListener("click", function () {
+      gridCell.classList.remove("hover");
+    });
+  }
+}
 
 generateGrid(defaultGrid);
-
-//Add a button on the top of the screen 
-// that will send the user a popup asking 
-// for the number of squares per side for 
-// the new grid. Once entered, the existing 
-// grid should be removed, and a new grid 
-// should be generated in the same total 
-// space as before (e.g., 960px wide) so 
-// that you’ve got a new sketch pad.
